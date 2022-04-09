ae914e6aebc92bca08f485fbc19f3952b4bdd568
dzianis-dashkevich <dzianis.dashkevich.us@gmail.com>
2022-04-09 07:30:23 +0000

    add styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<div class="todos">
+    <h1>{title}</h1>
+
+    <form action="" method="" class="new">
+        <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to a todo">
+    </form>
+    
+    <TodoItem></TodoItem>
+    <TodoItem></TodoItem>
+    <TodoItem></TodoItem>
+</div>
+
+<script>
+    import TodoItem from "$lib/todo-item.svelte";
+
+    const title = 'Todos';
+</script>
+
+<style>
+    .todos {
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto;
+    }
+
+    .new {
+        margin: 0 0 0.5rem 0;
+    }
+
+    .new input {
+        font-size: 28px;
+        width: 100%;
+        padding: 0.5em 1em 0.3em 1em;
+        border-radius: 8px;
+        background: rgba(255, 255, 255, 0.05);
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    .todos :global(input) {
+        border: 1px solid transparent;
+    }
+
+    .todos :global(input:focus-visible) {
+        box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
+        border: 1px solid #ff3e00 !important;
+        outline: none;
+    }
+</style>
